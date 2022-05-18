0445ee077f4c40c6c8c862b200746fc5dc48557c
Milos Milijasevic <milosrm20005@gmail.com>
2022-05-18 11:46:46 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+const buttonChangeBackground = document.querySelector(`.change-background`);
+const inputFirst = document.querySelector(`.input-first`);
+const inputSecond = document.querySelector(`.input-second`);
+const buttonCopyFirst = document.querySelector(`.button-first`);
+const buttonCopySecond = document.querySelector(`.button-second`);
+
+
